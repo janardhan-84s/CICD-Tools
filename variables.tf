e7f0b96eaa27a9c1fa1efeaaed63263e549324a9
Janardhan <janardhanj265@gmail.com>
2025-08-19 09:47:04 +0530

    terraform using CICD tools

---
+++ variables.tf
@@ -0,0 +1,21 @@
+
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_name" {
+  type        = string
+  default     = "jana-84s.site"
+  description = "description"
+}
+
+variable "zone_id" {
+  type        = string
+  default     = "Z06819642DWT6M5HGE8YJ"
+  description = "description"
+}
+
